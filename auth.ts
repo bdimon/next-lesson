2351729c4fb70514937df31e94a7f5a6dddfb434
bdimon <dmytr0b@yahoo.com>
2024-08-15 14:13:12 +0200

    Updating the login form

--- auth.ts
+++ auth.ts
@@ -36,7 +36,7 @@ export const { auth, signIn, signOut } = NextAuth({
 
             if (passwordsMatch) return user;
           }
-          console.log('Invalid credential');
+          console.log('Invalid credentials');
           
           return null;
       },
